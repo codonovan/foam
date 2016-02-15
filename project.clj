18795401707b00067df5a0bc4e4279a11bb464f7
Corey O'Donovan <corey.odonovan@gmail.com>
2016-02-15 18:43:32 +0000

    Added pandect dependency for adler32 implementation

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://github.com/arohner/foam"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [pandect "0.5.4"]]
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
                                      :creds :gpg}]])
